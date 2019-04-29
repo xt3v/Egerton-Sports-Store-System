0523afd2b088c4d35228e57763c1a6a244719c75
sulemaez <steve.oyeyo@gmail.com>
2019-04-29 20:45:37 +0300

    java

---
+++ src/main/java/com.Sports/Login.java
@@ -0,0 +1,17 @@
+
+package com.Sports;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/login")
+public class Login extends HttpServlet{
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("storekeeper.html");
+    }
+}
