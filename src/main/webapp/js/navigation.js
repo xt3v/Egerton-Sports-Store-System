23f9178866837bf7b85514264b5ecc558f6bf801
sulemaez <steve.oyeyo@gmail.com>
2019-05-08 11:48:37 +0300

    students 1

--- src/main/webapp/js/navigation.js
+++ src/main/webapp/js/navigation.js
@@ -4,22 +4,28 @@ addScript("js/templates/storekeeper/clearstudent.js")
 addScript("js/templates/storekeeper/returnitems.js")
 addScript("js/templates/storekeeper/recoverlostitems.js")
 
-function addNewItem(){
-   addToMain(addNewItemTemplate)
-}
-function recoverLostItems(){
-    addToMain(recoverLostItemsTemplate)
-    setupRecoverLostItemsTemplate()
-}
-
 function registerStudent(){
 	addToMain(registerStudentTemplate)
 }
+
 function clearStudent(){
 	addToMain(clearStudentTemplate)
 }
 
 
+function recoverLostItems(){
+    addToMain(recoverLostItemsTemplate)
+    setupRecoverLostItemsTemplate()
+}
+
+function addNewItem(){
+   addToMain(addNewItemTemplate)
+}
+
+function returnItems(){
+   addToMain(returnItemTemplate)
+   setupReturnedItemsTemplate()
+}
 
 function addScript(path){
     var imported = document.createElement('script');
